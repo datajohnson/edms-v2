6fc33e2a2cb13955495f0f67223fe97a21a7f95b
Michael Johnson <michael@icefoganalytics.com>
2020-11-24 15:42:07 -0700

    Added config for file patj

--- src/config.ts
+++ src/config.ts
@@ -22,3 +22,5 @@ export const DB_USER = process.env.DB_USER || "postgres";
 export const DB_PASS = process.env.DB_PASS || "password";
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PORT = process.env.DB_PORT || "5432";
+
+export const FILE_PATH = process.env.FILE_PATH || __dirname + "\\files";
