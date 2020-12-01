6785de08b00dd292dc9503819db87b9fe3fc6c44
Michael Johnson <michael@icefoganalytics.com>
2020-12-01 14:27:18 -0700

    Adding email libraries and config

--- src/config.ts
+++ src/config.ts
@@ -23,6 +23,10 @@ export const DB_PASS = process.env.DB_PASS || "password";
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PORT = process.env.DB_PORT || "5432";
 
+export const EMAIL_HOST = process.env.EMAIL_HOST || "";
+export const EMAIL_USER = process.env.EMAIL_USER || "";
+export const EMAIL_PASS = process.env.EMAIL_PASS || "";
+
 export const FILE_PATH = process.env.FILE_PATH || __dirname + "\\files\\";
 export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3";
 export const ENCRYPTION_IV = process.env.ENCRYPTION_IV || "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3";
