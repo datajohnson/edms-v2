453ecad5cae026d4fd16c2aa30fe36aa9d0fa584
Michael Johnson <michael@icefoganalytics.com>
2020-12-01 13:06:18 -0700

    switching primary keys to bigint

--- src/data/migrations/create-error-log.ts
+++ src/data/migrations/create-error-log.ts
@@ -2,7 +2,7 @@ import Knex from "knex";
 
 exports.up = function(knex: Knex, Promise:any) {
     return knex.schema.createTable("error_log", function(t) {
-        t.increments('id').unsigned().primary();
+        t.bigIncrements('id').unsigned().primary();
         t.dateTime('create_date').notNullable().defaultTo(knex.raw("CURRENT_TIMESTAMP"));
         t.string('service_name').notNullable();
         t.text('message').nullable();
